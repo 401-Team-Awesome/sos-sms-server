db6be378ddbdb9b4b711b0632e2766a147f39758
Carl Olson <colson10@gmail.com>
2018-05-14 15:53:37 -0700

    removed test env

--- src/__test__/lib/test.env.js
+++
@@ -1,5 +0,0 @@
-process.env.NODE_ENV = 'development';
-process.env.PORT = 7000;
-process.env.MONGODB_URI = 'mongodb://localhost/testing';
-process.env.TWILIO_NUMBER='+15005550006';
-// testing gitignore
