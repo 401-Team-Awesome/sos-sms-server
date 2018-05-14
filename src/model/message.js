f1d0d6cec7d69fbaecb794f47a302e354d6e7958
cara ottmar <cara.ottmar@gmail.com>
2018-05-14 14:08:36 -0700

    models for account and message made, POST routers for message and account also complete.

---
+++ src/model/message.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import HttpError from 'http-errors';
+import Account from '../model/account'; // eslint-disable-line
+
+const messageSchema = mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  error: {
+    type: Number,
+    required: true,
+  },
+  message: {
+    type: String,
+  },
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'account',
+  },
+});
+
+export default mongoose.model('message', messageSchema);
