a4fde79fd8f357e7d192cb873924bf5b101ef444
sarasaurus <bixler.s.e@gmail.com>
2018-05-14 14:03:18 -0700

    adding npm package

---
+++ src/lib/sos-sms-middleware.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import superagent from 'superagent';
+
+const apiURL = `http://localhost:${process.env.PORT}`;
+
+export default sossms = (error, userID, message, next) => {
+  return superagent.post(`${apiURL}/messages/:${userID}`)
+    .send({ error, message })
+    .then(()=> next())
+    .catch(next)
+}
