70c47b3daf442b9219ca0cebdf21705cb1861f5d
cara ottmar <cara.ottmar@gmail.com>
2018-05-15 12:05:54 -0700

    added bearer and basic auth and started scaffolding authorization.

--- build/__test__/lib/test.env.js
+++ build/__test__/lib/test.env.js
@@ -3,7 +3,7 @@
 process.env.NODE_ENV = 'development';
 process.env.PORT = 7000;
 process.env.MONGODB_URI = 'mongodb://localhost/test';
-process.env.PHONE_NUMBER = '+12068535906';
-process.env.TWILIO_NUMBER = '+12062079846';
-process.env.TWILIO_ACCOUNT_SID = 'ACea4c478b35ec70633bedc81b84af7ddd';
-process.env.TWILIO_AUTH_TOKEN = '6235383d7bad1009dd638c1371b3e821';
+process.env.PHONE_NUMBER = 
+process.env.TWILIO_NUMBER = 
+process.env.TWILIO_ACCOUNT_SID = 
+process.env.TWILIO_AUTH_TOKEN = 
--- src/model/account.js
+++ src/model/account.js
@@ -1,13 +1,70 @@
 'use strict';
 
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import jsonWebToken from 'jsonWebToken';
+
+const HASH_ROUNDS = 8;
+const TOKEN_SEED_LENGTH = 128;
 
 const accountSchema = mongoose.Schema({
+  passwordHash: {
+    type: String,
+    required: true,
+  },
   userPhoneNumber: {
     type: String, 
     required: true,
     // unique: true,
   },
+  tokenSeed: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  timeStamp: {
+    type: Date, 
+    default: () => Date(),
+  },
 });
 
-export default mongoose.model('account', accountSchema);
+function pVerifyPassword(password) {
+  return bcrypt.compare(password, this.passwordHash)
+    .then((result) => {
+      if (!result) {
+        throw new Error('400', 'Password error AUTH - incorrect data');
+      }
+      return this;
+    });
+}
+
+function pCreateToken() {
+  this.tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+  return this.save()
+    .then((account) => {
+      return jsonWebToken.sign({ tokenSeed: account.tokenSeed }, process.env.HASH_SECRET_STRING);
+    });
+}
+
+accountSchema.methods.pCreateToken = pCreateToken;
+accountSchema.methods.pVerifyPassword = pVerifyPassword;
+
+const Account = mongoose.model('account', accountSchema);
+
+Account.create = (username, email, password) => {
+  return bcrypt.hash(password, HASH_ROUNDS)
+    .then((passwordHash) => {
+      password = null;
+      const tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+      return new Account({
+        passwordHash,
+        userPhoneNumber,
+        tokenSeed,
+        timeStamp,
+      }).save();
+    });
+};
+
+
+export default Account;
