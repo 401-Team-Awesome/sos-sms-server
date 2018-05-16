d6369256385096f821a15d7635fd8ee02229a6c7
sarasaurus <bixler.s.e@gmail.com>
2018-05-16 16:22:58 -0700

    added auth and switched to deploy settings

--- src/lib/sos-sms-middleware.js
+++ src/lib/sos-sms-middleware.js
@@ -2,8 +2,8 @@
 
 import superagent from 'superagent';
 
-const apiURL = `http://localhost:${process.env.PORT}`;
-// const apiURL = 'https://sos-sms.herokuapp.com';
+// const apiURL = `http://localhost:${process.env.PORT}`;
+const apiURL = 'https://sos-sms.herokuapp.com';
 
 export default (error, userID, message) => {
   console.log(`${apiURL}/api/messages/${userID}`, 'url in sossms middleware');
